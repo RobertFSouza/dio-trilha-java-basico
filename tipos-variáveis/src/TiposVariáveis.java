10ea6d0190cf7561c96b29005c5758d03395fc40
Robert Souza <robertsouzarvj@gmail.com>
2023-06-11 02:18:44 -0300

    mudança e teste

---
+++ tipos-variáveis/src/TiposVariáveis.java
@@ -0,0 +1,17 @@
+public class TiposVariáveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.33;
+        int numero = 5;
+
+        numero = 15;
+        System.out.println(numero);
+
+
+
+
+        
+    }
+        
+    }
+    
+
